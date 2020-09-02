fc528322059e0fe9ede797638971d375e12a36ab
Pari Shahbazi <60629905+paridev@users.noreply.github.com>
2020-09-02 13:26:21 +0200

    update
    text

--- connecting_to_nasdaq.py
+++ connecting_to_nasdaq.py
@@ -9,7 +9,7 @@ print("Please enter the year:")
 my_date = input()
 if my_date == "":
 
-    print("ho hoo ho There is no date and we can not support anymore")
+    print("hoooooooooooooo hoo ho There is no date and we can not support anymore")
 
 else:
     root = Tk()
